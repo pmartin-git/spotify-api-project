0b0747022194853bc8531b9e8d9e52c0ef1b0a4f
pmartin-git <p.martin.9089@gmail.com>
2025-08-23 20:24:55 -0500

    get dbt dag working locally

--- airflow/dags/run_dbt_project.py
+++ airflow/dags/run_dbt_project.py
@@ -2,43 +2,38 @@ from airflow.sdk import dag, task
 from airflow.providers.standard.operators.bash import BashOperator
 from datetime import datetime
 
-DBT_PROJECT_PATH = "/home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/"
-
 @dag
 def run_dbt_project():
 
-    run_dbt_deps = BashOperator(
-        task_id="run_dbt_deps",
-        bash_command=
-            "cd /home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/; \
-            eval $(poetry env activate); \
-            dbt deps"
+    dbt_deps = BashOperator(
+        task_id="dbt_deps",
+        bash_command="cd /opt/airflow/dbt/spotify_dbt_project/ && dbt deps"
     )
 
-    run_dbt_seed = BashOperator(
-        task_id="run_dbt_seed",
-        bash_command=
-            "cd /home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/ \
-            && eval $(poetry env activate) \
-            && dbt seed"
+    dbt_seed = BashOperator(
+        task_id="dbt_seed",
+        bash_command="cd /opt/airflow/dbt/spotify_dbt_project/ && dbt seed"
+            # "cd /home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/ \
+            # && eval $(poetry env activate) \
+            # && dbt seed"
     )
 
-    run_dbt_run = BashOperator(
-        task_id="run_dbt_run",
-        bash_command=
-            "cd /home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/; \
-            eval $(poetry env activate); \
-            dbt run"
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command="cd /opt/airflow/dbt/spotify_dbt_project/ && dbt run"
+            # "cd /home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/; \
+            # eval $(poetry env activate); \
+            # dbt run"
     )
 
-    run_dbt_test = BashOperator(
-        task_id="run_dbt_test",
-        bash_command=
-            "cd /home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/ \
-            && eval $(poetry env activate) \
-            && dbt test"
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command="cd /opt/airflow/dbt/spotify_dbt_project/ && dbt test"
+            # "cd /home/pmartin/projects/spotify_api_project/dbt/spotify_dbt_project/ \
+            # && eval $(poetry env activate) \
+            # && dbt test"
     )
 
-    run_dbt_deps >> run_dbt_seed >> run_dbt_run >> run_dbt_test
+    dbt_deps >> dbt_seed >> dbt_run >> dbt_test
 
 run_dbt_project()
