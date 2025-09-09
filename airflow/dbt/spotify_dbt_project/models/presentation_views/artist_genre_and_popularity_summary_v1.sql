03d47be8a4866a4118f07990d7efcafd399e291b
pmartin-git <p.martin.9089@gmail.com>
2025-09-09 00:18:09 -0500

    adjust model and workflow

--- airflow/dbt/spotify_dbt_project/models/presentation_views/artist_genre_and_popularity_summary_v1.sql
+++ airflow/dbt/spotify_dbt_project/models/presentation_views/artist_genre_and_popularity_summary_v1.sql
@@ -3,8 +3,7 @@ select
     artist_name,
     genre_categories,
     genre_category_sub_genres,
-    artist_popularity,
-    current_timestamp as table_created_at
+    artist_popularity
 from
     {{ ref('artist_genre_and_popularity_summary_t1') }}
 order by
